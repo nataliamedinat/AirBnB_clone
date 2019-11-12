473bb3d2ac7763ee714fb650978236f731f7cff5
nataliamedinat <nataliamedinabotero@gmail.com>
2019-11-12 15:52:00 +0000

    Trying to fixed the dictionary

--- models/__init__.py
+++ models/__init__.py
@@ -10,8 +10,13 @@ from models.place import Place
 from models.review import Review
 
 
-classes = {"BaseModel": BaseModel, "User": User, "State": State,
-           "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+classes = {"BaseModel": BaseModel,
+           "User": User,
+           "State": State,
+           "City": City,
+           "Amenity": Amenity,
+           "Place": Place,
+           "Review": Review}
 
 storage = FileStorage()
 storage.reload()
