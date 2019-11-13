1fc9b58fb78ed49727409291e86f7e2f359b25a6
dabrabgellak <barbaracalle10i3@gmail.com>
2019-11-13 19:53:17 +0000

    created_at instead of timedate.now()

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Module Base """
 
-
 from datetime import datetime
 from uuid import uuid4
 import models
@@ -10,6 +9,7 @@ import models
 class BaseModel:
     """ Defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
+        """ Initialization """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
@@ -24,7 +24,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = created_at
             models.storage.new(self)
 
     def __str__(self):
